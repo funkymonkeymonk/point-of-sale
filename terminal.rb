f682fdf214f8d57134724790eaf2c08c82ff16d1
FunkyMonkeyMonk <monkey@buildingbananas.com>
2014-08-02 13:06:35 -0400

    Passed: Terminal scans an item and adds the price to the total

--- terminal.rb
+++ terminal.rb
@@ -2,19 +2,32 @@
 
 class Terminal
   def initialize()
+    # Positional Constants for product array
+    @PRICE = 0
+    @BULK_QUANTITY = 1
+    @BULK_DISCOUNT = 2
+
     @products = {}
-    @total = 0
+    @cart = []
   end
 
   def total
-    @total
+    @cart.reduce(0) do |sum, code|
+      sum + @products[code][@PRICE]
+    end
   end
 
   def add_product(code, price, bulk_quantity, bulk_discount)
+    # Not certain if I should leave each product as an array or if I should make
+    # this it's own very simple class
     @products[code] = [price, bulk_quantity, bulk_discount]
   end
 
   def get_products()
     @products
+  end
+
+  def scan(code)
+    @cart << code
   end
 end
