ce3d83f956525cd4e47349a103e21cf767bb2bfe
FunkyMonkeyMonk <monkey@buildingbananas.com>
2014-08-02 14:33:42 -0400

    Modified: Cleaned up tests and fixed bulk test

--- terminal_spec.rb
+++ terminal_spec.rb
@@ -6,25 +6,24 @@ describe Terminal do
   context "with no product prices loaded" do
     term = Terminal.new
 
-    it "has total of 0 for a new terminal" do
+    it "has total of 0 for a new terminal." do
       expect(term.total).to eq(0)
     end
 
-    it "stores new products by their code, price, bulk quantity, and bulk discount" do
+    it "stores new products by their code, price, bulk quantity, and bulk discount." do
       # Product A costs 2 dollars each or 4 for $7
       term.add_product('A',2,4,7)
       expect(term.get_products).to eql({'A' => [2,4,1]}) 
     end
 
-    it "scans an item and adds the price to the total" do
+    it "scans an item and adds the price to the total." do
       term.scan('A')
       expect(term.total).to eq(2)
     end
 
-    it "deducts bulk discounts when the bulk quantity of an item has been reached" do
-      7.times { term.scan('A') }
+    it "deducts bulk discounts when the bulk quantity of an item has been reached." do
+      10.times { term.scan('A') }
       expect(term.total).to eq(20)
     end
-
   end
 end
