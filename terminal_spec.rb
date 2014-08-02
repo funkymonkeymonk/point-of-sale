936ce87bb89a9219aecb1fc671920052eb7e51c5
FunkyMonkeyMonk <monkey@buildingbananas.com>
2014-08-02 16:16:20 -0400

    Modified: Inputting all prices a floating point numbers

--- terminal_spec.rb
+++ terminal_spec.rb
@@ -7,39 +7,39 @@ describe Terminal do
     term = Terminal.new
 
     it "has total of 0 for a new terminal." do
-      expect(term.total).to eq(0)
+      expect(term.total).to eq(0.00)
     end
 
     it "stores new products by their code, price, bulk quantity, and bulk discount." do
       # Product A costs 2 dollars each or 4 for $7
-      term.add_product('A',2,4,7)
-      expect(term.get_products).to eql({'A' => [2,4,1]}) 
+      term.add_product('A',2.00,4,7.00)
+      expect(term.get_products).to eql({'A' => [2.00,4,1.00]}) 
     end
 
     it "scans an item and adds the price to the total." do
       term.scan('A')
-      expect(term.total).to eq(2)
+      expect(term.total).to eq(2.00)
     end
 
     it "deducts bulk discounts when the bulk quantity of an item has been reached." do
       10.times { term.scan('A') }
-      expect(term.total).to eq(20)
+      expect(term.total).to eq(20.00)
     end
 
     it "stores new products with no bulk discounts." do
-      term.add_product('B',12)
-      expect(term.get_products).to eql({'A' => [2,4,1], 'B' => [12,1,0]})
+      term.add_product('B',12.00)
+      expect(term.get_products).to eql({'A' => [2.00,4,1.00], 'B' => [12.00,1,0.00]})
     end
   end
 
 
   context "with product prices loaded" do
-    products = {'A' => [2,4,7], 'B' => [12], 'C' => [1.25,6,6], 'D' => [0.15]}
+    products = {'A' => [2.00,4,7.00], 'B' => [12.00], 'C' => [1.25,6,6.00], 'D' => [0.15]}
 
     it "can accept a hash of pricing data." do
       term = Terminal.new
       term.set_pricing(products)
-      expect(term.get_products).to eql({'A' => [2,4,1], 'B' => [12,1,0], 'C' => [1.25,6,1.50], 'D' => [0.15,1,0]})
+      expect(term.get_products).to eql({'A' => [2.00,4,1.00], 'B' => [12.00,1,0.00], 'C' => [1.25,6,1.50], 'D' => [0.15,1,0.0]})
     end
 
   
