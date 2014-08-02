c9c63acbb6db1f4f8e1204de20c33eed490e2c83
FunkyMonkeyMonk <monkey@buildingbananas.com>
2014-08-02 13:19:41 -0400

    Added: Terminal deducts a bulk discount when the bulk quantity of an item has been reached

--- terminal_spec.rb
+++ terminal_spec.rb
@@ -3,21 +3,29 @@
 require_relative 'terminal'
 
 describe Terminal do
+  context "with no product prices loaded" do
+    term = Terminal.new
 
-  term = Terminal.new
+    it "has total of 0 for a new terminal" do
+      expect(term.total).to eq(0)
+    end
 
-  it "has total of 0 for a new terminal" do
-    expect(term.total).to eq(0)
+    it "stores new products by their code, price, bulk quantity, and bulk discount" do
+      # Product A costs 2 dollars each or 4 for $7
+      term.add_product('A',2,4,7)
+      expect(term.get_products).to eql({'A' => [2,4,7]}) 
+    end
+
+    it "scans an item and adds the price to the total" do
+      term.scan('A')
+      expect(term.total).to eq(2)
+    end
+
+    it "deducts a bulk discount when the bulk quantity of an item has been reached" do
+      term.scan('A')
+      term.scan('A')
+      term.scan('A')
+      expect(term.total).to eq(7)
+    end
   end
-
-  it "stores new products by their code, price, bulk quantity, and bulk discount" do
-    # Product A costs 2 dollars each or 4 for $7
-    term.add_product('A',2,4,7)
-    expect(term.get_products).to eql({'A' => [2,4,7]}) 
-  end
-
-  it "scans an item and adds the price to the total" do
-    term.scan('A')
-    expect(term.total).to eq(2)
-  end  
 end
